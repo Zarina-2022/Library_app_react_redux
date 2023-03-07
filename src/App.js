eb98c116914e4c592dee9bc30351cd27c3080477
Zarina-2022 <sekerdagzarina@gmail.com>
2023-03-07 16:10:57 +0100

    router structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const allStates = useSelector((state) => state);
   return (
-    <div>
-      <h1>Library app</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+// <Route path="*" element={<NotFoundPage />} />
+// bu sayfayi diger sayfalarin en altina yazariz.
+// Yildizin(*) anlami: diger path'lere uymiyorsa bu sayfa gosterilsin 
